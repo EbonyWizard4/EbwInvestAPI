98fc67fb5c1d748db0cf5d58f28a385a37c0922e
EbonyWizard4 <antonio.jhone@hotmail.com>
2024-03-01 17:22:09 -0300

    make pages and define rout and base.html

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
+
 @app.route('/index')
 def index():
-    return "Hello World! :D"
+    nome = 'Jhone'
+    return render_template('index.html', nome=nome)
+
+@app.route('/contatos')
+def contatos():
+    return render_template('contatos.html')
