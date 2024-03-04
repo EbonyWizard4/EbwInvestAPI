2e4e313a870059111b7dfda3fe37dd2e86d54e83
EbonyWizard4 <antonio.jhone@hotmail.com>
2024-03-04 17:03:08 -0300

    config sqlite3 db

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -23,8 +23,11 @@ def create_app(test_config=None):
         pass
 
     #a simple page that says hello
-    @app.route('/hello')
+    @app.route('/')
     def hello():
         return 'Hello World! :D'
+    
+    from . import db
+    db.init_app(app)
     
     return app
